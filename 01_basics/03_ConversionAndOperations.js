748e6aef5f63e1e70b49db62b500653c7de74e44
Vaishnav Patil <vaishnavpatil405@gmail.com>
2025-05-29 06:50:42 +0000

    Conversion of data types

---
+++ 01_basics/03_ConversionAndOperations.js
@@ -0,0 +1,23 @@
+// let score = "33abc"
+// console.log(typeof score);
+// let valueInNumber = Number (score);
+// console.log(valueInNumber);
+// console.log(typeof (valueInNumber));
+
+/*
+    "33" => 33
+    "33abc" => Nan (Not a Number) but type is Number only
+    true => 1; false =>0;
+*/
+
+let NumisloggedIn = 1;
+let BoolLoggedIn = Boolean(NumisloggedIn)
+console.log("Num -" ,NumisloggedIn ,"Boolean - " ,BoolLoggedIn)
+
+//1 => True ; 0=>false
+//"" => False ; "Vaishnav" => True
+
+
+
+
+
