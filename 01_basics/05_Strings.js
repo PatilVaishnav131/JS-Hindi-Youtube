b6a241d00440cf650315d658a780a2dad657025c
Vaishnav Patil <vaishnavpatil405@gmail.com>
2025-05-30 15:46:26 +0000

    strings started

---
+++ 01_basics/05_Strings.js
@@ -0,0 +1,14 @@
+const name = "vaishnav"
+const count = 40
+
+//traditional way 
+// console.log(name + count + "value");
+
+//Modern way 
+console.log(`Hello my name is ${name} and my repo count is ${count}`)
+//String Interpolation
+const myname = new String ("Vaishnav")  //class is created
+console.log(myname[0]);
+
+//Visit console and see all the methods
+
