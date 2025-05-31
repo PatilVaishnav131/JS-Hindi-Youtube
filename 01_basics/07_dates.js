605263f9f1e855c86d3b509c31fd93272dbcf885
Vaishnav Patil <vaishnavpatil405@gmail.com>
2025-05-31 08:32:57 +0000

    date and time

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,13 @@
+let date = new Date()
+console.log(date.toLocaleString());
+//console.log(date.now());
+
+let myts = Date.now();
+console.log(myts);
+
+let myCreatedDate = new Date(2023,0,23)
+console.log(myCreatedDate.toLocaleString())
+console.log(myCreatedDate.toDateString())
+
+
+
