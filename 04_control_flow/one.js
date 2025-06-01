20407ee62ce2356f76301819cf578550a2403a92
Vaishnav Patil <vaishnavpatil405@gmail.com>
2025-06-01 15:59:36 +0000

    comparator

---
+++ 04_control_flow/one.js
@@ -0,0 +1,5 @@
+//control flow 
+if(2=="2")
+        console.log("Executed");  //only compares the value;
+if(2==="2") // === strict equalto !== strictly not equal to
+        console.log("Typed as well as the value is also is matched")
