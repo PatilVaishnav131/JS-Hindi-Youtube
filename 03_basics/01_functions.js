a79b369c306747cfa6d87ba894cd6c64922bc282
Vaishnav Patil <vaishnavpatil405@gmail.com>
2025-06-01 07:31:42 +0000

    started functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,14 @@
+function printName(){
+    console.log("vaishnav");
+}
+
+//printName //Function REference
+//printName(); //Function Execution 
+
+function add(number1 , number2){
+    return (number1 + number2)
+}
+const sum = add(4,undefined)
+console.log(sum);
+
+
