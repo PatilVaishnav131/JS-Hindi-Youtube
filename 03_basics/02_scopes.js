1d4510d72879d4d08b486d55a991a4698f2a85e9
Vaishnav Patil <vaishnavpatil405@gmail.com>
2025-06-01 12:45:07 +0000

    nested scoping and hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -6,12 +6,37 @@ if(true){ //block scope the variables declared using let and cosnt are limited t
     let a = 10
     var b = 20
     const c = 30
-    console.log(`This is the inner a ${a}`);
+    //console.log(`This is the inner a ${a}`);
     
 }
 //console.log(a);  it says not defined 
-console.log(b);
+//console.log(b);
 //console.log(c);
-console.log(`This is the outer a : ${a}`)
+//console.log(`This is the outer a : ${a}`)
 
 
+// **************Nested Scoping**************** \\
+function one(){
+    const name = "vaishnav"
+    function two(){
+        const website = "Youtube"
+        console.log(name.concat(" ",website))
+    }
+    //console.log(website)
+    return two()
+}
+one()
+
+//*******  INTERESTING EXAMPLE ************/
+//Hoisting 
+console.log(addOne(5)); //no error if called before declaring
+
+function addOne(num){
+    return num + 1
+}
+
+//console.log(addTwo(6)) // Gives an error if called before declaring 
+const addTwo = function(num){
+    return num + 2
+}
+console.log(addTwo(8))
