55d7f92d325146698d7e1f3b96b0e3d4de30e53a
Vaishnav Patil <vaishnavpatil405@gmail.com>
2025-06-02 07:04:17 +0000

    while and do while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,11 @@
+let i =0
+while(i<=10){
+    console.log(`Value of the i is ${i}`)
+    i = i +2
+}
+
+let score = 11
+do{
+    console.log(`The score is ${score}`)
+    score++
+}while(score<=10) //even the condition is false the loop is executed once
