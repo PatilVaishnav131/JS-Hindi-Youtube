96b036fbc53f334b3e66463924a3cdecf4e9021a
Vaishnav Patil <vaishnavpatil405@gmail.com>
2025-06-08 07:36:19 +0000

    how the new keyword works !

--- 10_classesAndOOPs/oops.js
+++ 10_classesAndOOPs/oops.js
@@ -1,4 +1,12 @@
 //create a constructor
+//How “new” keyword works:-
+/*
+1. "new" keyword creates a new empty object or we can say instance.
+2. constructor function is called that will have all the arguments
+3. "this" keyword is injected in a new instance with all the arguments
+4. We will receive the new object instance with all the arguments
+
+*/
 function User(username , loginCount ="1" , isLoggedIn = true){
     this.username = username
     this.username = loginCount
